d9688e272b2352944235995c6d60213f1d6c0171
Andrey Kozikov <nwisdom@mail.ru>
2024-09-28 19:42:53 +0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,53 @@
+package org.example;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+/*
+-- Создаем схему (базу данных)
+CREATE DATABASE hibernate;
+
+-- Выбираем созданную схему
+USE hybernate;
+
+-- Создаем таблицу Person
+CREATE TABLE Person (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    age INT NOT NULL
+);
+
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        // Создание объекта Configuration и настройка свойств программно
+        Configuration configuration = new Configuration();
+        configuration.configure(); // Здесь можно явно не указывать XML, если он находится в корне проекта
+        configuration.addAnnotatedClass(Person.class); // Добавляем класс сущности
+
+        // Создание ServiceRegistry
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()) // Применение настроек конфигурации
+                .build();
+
+        // Создание SessionFactory с использованием ServiceRegistry
+        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+        // Открытие сессии и выполнение CRUD-операций
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            Person ivan = new Person("Ivan", 23);
+            session.save(ivan);
+            Person vasiliy = new Person("Vasiliy", 37);
+            session.save(vasiliy);
+        } finally {
+            session.getTransaction().commit();
+            sessionFactory.close();
+        }
+    }
+}
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Person")
+
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private int age;
+
+    //region Геттеры и сеттеры
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    //endregion
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                "}\n";
+    }
+}
